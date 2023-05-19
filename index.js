8e8cb9839c4a10739d5a8521ba8db36913755763
Luis <garcia.luis.enrique19@gmail.com>
2023-05-18 22:02:05 -0700

    adding classes anf methods

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Car {
+    constructor( make, model, year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+
+
+    }
+
+    getDescription (){
+       
+        return this.make+ " model " + this.model + " year " + this.year;
+    }
+}
+
+
+class ElectricCar extends Car {
+
+    constructor( make, model, year, range){
+        super(make, model, year);
+        this.range = range;
+    }
+
+    getDescription(){
+        
+        return super.getDescription() + " range " + this.range ;
+    }
+
+
+}
+
+
+const myCar = new ElectricCar("Tesla","Model S","2019","300");
+
+
+
+console.log(myCar.getDescription());
+
+
 
